07f4b22da6a75e4469fe55e674e77be9ff03ca0b
Juno Julius <yunoyulius@gmail.com>
2018-07-23 21:27:03 +0200

    lorem

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+        let pic = {
+          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        };
     return (
       <View style={styles.container}>
 
       < Text > A Login</Text >
       < Text > A QR scanner </Text>
       < Text > A Session View </Text>
-      
+            <Image source={pic} style={{width: 193, height: 110}}/>
 
 
 
